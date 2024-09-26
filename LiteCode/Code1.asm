b29870507cacf7d0765ec5b357564752f0058446
French <152695796+FrenchPower7@users.noreply.github.com>
2024-09-26 14:36:35 +0200

    Add files via upload

---
+++ LiteCode/Code1.asm
@@ -0,0 +1,43 @@
+; In french :
+;Demandez une valeur à l'utilisateur
+;Renvoie la valeur à l'utilisateur
+
+extern printf ;affichage
+extern scanf ;asking
+
+
+global main
+
+section .data
+
+	fmt_scan: db "%d",0
+	fmt_print: db "Vous avez entré la valeur : %d",10,0
+	ask: db "Entrez une valeur entière :",0
+
+section .bss
+
+	reponse : resb 1
+
+section .text
+main:
+
+	push rbp
+	mov rdi,ask ; affichage de la question
+	mov rax,0 ; pas d'arguments flottants
+	call printf
+
+	mov rdi,fmt_scan ; format du scanf
+	mov rsi,reponse ; adresse de la variable où stocker la saisie	
+	mov rax,0 ; pas d'arguments flottants
+	call scanf
+	mov rdi,fmt_print ; format du second printf
+	movzx rsi,byte[reponse] ; valeur de l'argument entier à afficher
+	mov rax,0 ; pas d'arguments flottants
+	call printf
+	pop rbp
+
+
+mov rax, 60 ; good way to closse the programe
+mov rdi, 0
+syscall
+ret ; return (nothing here)
